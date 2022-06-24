281e743b6eef3eff0a3c92dd9536c7cde37900b4
lvidal1 <lvidal910@gmail.com>
2022-06-24 01:00:40 -0500

    Add demo call

--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
-import csv
+from web.MemoryKings import MemoryKings
 
-URL = "https://www.memorykings.pe/resultados/rtx"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-catalog = soup.find("ul", class_="products")
-products = catalog.find_all("li")
-
-
-def format_price(raw_price):
-    return raw_price.split("ó")[1].replace("S/", "").strip()
-
-
-f = open('./data-memory-kings.csv', 'w')
-writer = csv.writer(f)
-
-for product in products:
-    link_element = product.find('a')
-    image_element = product.find("img")
-    title_element = product.find("div", class_="title")
-    price_element = product.find("div", class_="price")
-
-    row_product = [
-        title_element.text.strip(),
-        link_element['href'],
-        image_element['src'],
-        format_price(price_element.text)
-    ]
-
-    print(row_product)
-    writer.writerow(row_product)
-
-f.close()
+MemoryKingsCall = MemoryKings("https://www.memorykings.pe/resultados/rtx")
+MemoryKingsCall.run()
