0f964a144de7ed7f8b83995cffff790266c3bd8c
Leonardo Vidal <lvidal910@gmail.com>
2022-06-24 23:55:55 -0500

    [Mongo] Add test implementation of pymongo (#1)
    * Add test implementation of pymongo
    
    * Comment

---
+++ utils/mongo.py
@@ -0,0 +1,18 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# Client connection
+# It is secured by network access :)
+client = MongoClient(
+    "mongodb+srv://root:z5lcBTFthHZiDsGn@products.fvdt9jr.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+
+db = client["scrapped_products"]
+
+col = db["test"]
+
+collist = db.list_collection_names()
+if "test" in collist:
+    print("The collection exists.")
+
+    dict = {"name": "Leo", "address": "Av. Camera Man"}
+    x = col.insert_one(dict)
